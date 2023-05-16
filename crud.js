b08c163d2fc17647952a44f210169d591494f143
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-16 20:22:13 -0300

    ajustando

--- crud.js
+++ crud.js
@@ -1,42 +1,42 @@
 document.querySelector("#salvar").addEventListener("click", cadastrar)
 
-let lista_tarefas = [] 
+let lista_filmes = [] 
 
 window.addEventListener("load", () => {
-    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas"))
-    lista_tarefas.forEach((tarefa) => {
-        document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+    lista_filmes = JSON.parse(localStorage.getItem("lista_filmes"))
+    lista_filmes.forEach((filme) => {
+        document.querySelector("#filmes").innerHTML += gerarCard(filme)
     })
 
 })
 
 function cadastrar(){
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
-    let titulo = document.querySelector("#titulo").value
-    let descricao = document.querySelector("#descricao").value
-    let pontos = document.querySelector("#pontos").value
-    let categoria = document.querySelector("#categoria").value
+    let Nome_do_Filme = document.querySelector("#Nome do Filme").value
+    let Elenco = document.querySelector("#Elenco").value
+    let nota = document.querySelector("#nota").value
+    let Avaliação = document.querySelector("#Avaliação").value
 
     const tarefa = {
-        titulo,
-        descricao,
-        pontos,
-        categoria,
+        Nome_do_Filme,
+        Elenco,
+        nota,
+        Avaliação,
     }
 
-    if (tarefa.titulo.length == 0) {
-        document.querySelector("#titulo").classList.add("is-invalid")
+    if (filme.Nome_do_Filme.length == 0) {
+        document.querySelector("#Nome do Filme").classList.add("is-invalid")
         return 
     }
 
     lista_tarefas.push(tarefa)
 
-   document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+   document.querySelector("#Filmes").innerHTML += gerarCard(tarefa)
 
-   document.querySelector("#titulo").value = ""
-   document.querySelector("#descricao").value = ""
+   document.querySelector("#Nome do Filme").value = ""
+   document.querySelector("#Elenco").value = ""
 
-   localStorage.setItem("lista_tarefas", JSON.stringify(lista_tarefas))
+   localStorage.setItem("lista_filmes", JSON.stringify(lista_filmes))
 
    modal.hide()
 
@@ -45,23 +45,23 @@ function apagar(botao){
     botao.parentNode.parentNode.remove()
 }
 
-function gerarCard(tarefa){
+function gerarCard(filme){
     return `  
 
     
         <div class="col-lg-3 col-md-6 col-12">
             <div class="card">
                 <div class="card-header">
-                    ${tarefa.titulo}
+                    ${filme.Nome_do_Filme}
                 </div>
                 <div class="card-body">
-                    <p class="card-text">${tarefa.descricao}.</p>
+                    <p class="card-text">${filme.Elenco}.</p>
                     <p>
                         <span class="badge text-bg-warning">
-                        ${tarefa.categoria}
+                        ${filme.Avaliação}
                         </span>
                     </p>
-                    <p>${tarefa.pontos}pts</p>
+                    <p>${filme.nota}pts</p>
                     <a href="#" class="btn btn-success">
                         <i class="bi bi-check-lg"></i>
                     </a>
